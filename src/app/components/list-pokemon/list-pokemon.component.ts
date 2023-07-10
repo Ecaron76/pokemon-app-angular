aaa8cd855f7ec250bab021dec64d23ff428f0caf
Ecaron76 <ecaronpro76@gmail.com>
2023-07-10 14:59:38 +0200

    Add files via upload

--- src/app/components/list-pokemon/list-pokemon.component.ts
+++ src/app/components/list-pokemon/list-pokemon.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { POKEMONS } from 'src/app/data/mock-pokemon-list';
 import { Pokemon } from 'src/app/models/pokemon';
 
@@ -8,4 +9,10 @@ import { Pokemon } from 'src/app/models/pokemon';
 })
 export class ListPokemonComponent {
   pokemonList: Pokemon[] = POKEMONS
+
+  constructor(private router: Router){}
+  
+  goToPokemon(pokemon: Pokemon){
+    this.router.navigate(['/pokemons', pokemon.id])
+  }
 }
